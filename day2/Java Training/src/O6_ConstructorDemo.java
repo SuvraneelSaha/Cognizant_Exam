1602492ea58200f37386a4cec4a9060b94e8774e
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-10-06 14:31:01 +0530

    Update O6_ConstructorDemo.java

--- day2/Java Training/src/O6_ConstructorDemo.java
+++ day2/Java Training/src/O6_ConstructorDemo.java
@@ -3,7 +3,7 @@ public class O6_ConstructorDemo {
         O6_Book book = new O6_Book();
         System.out.println(book);
 
-        O6_Book book1 = new Book("A101" ,"Let us C",1200.89);
+        O6_Book book1 = new O6_Book("A101" ,"Let us C",1200.89);
         System.out.println(book1);
     }
 }
@@ -15,5 +15,3 @@ public class O6_ConstructorDemo {
 // string index value compare
 // -1 means not there
 // the vowel is absent
-
-https://cognizant-extlive.kpoint.com/web/videos/gcc-3b0c9f95-f72e-4d05-8eab-50765049804f
